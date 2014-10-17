f3207f46a6f34133d84b0e77fcd63a498a0fbc20
Haralan Dobrev <hkdobrev@gmail.com>
2014-10-17 15:09:51 +0300

    Add FreezableSimpleCollectionTrait

---
+++ src/FreezableSimpleCollectionTrait.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Clippings\Freezable;
+
+/**
+ * @author    Haralan Dobrev <hkdobrev@gmail.com>
+ * @copyright 2014, Clippings Ltd.
+ * @license   http://spdx.org/licenses/BSD-3-Clause
+ */
+trait FreezableSimpleCollectionTrait
+{
+    use FreezableSimpleTrait;
+    use FreezableCollectionTrait;
+}
